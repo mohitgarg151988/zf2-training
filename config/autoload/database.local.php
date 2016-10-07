da00e62fed9497cd0f41ba2c38f7ff758870aad5
Mohit Kumar Gupta <mohit@osscube.com>
2016-10-07 18:11:09 +0530

    Database setting changes

---
+++ config/autoload/database.local.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+	'service_manager' => array(
+		'factories' => array(
+			'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory'
+		)
+	),
+	'db' => array(
+		'driver' => 'pdo',
+		'dsn' => 'mysql:dbname=zf2_training;host=localhost',
+		'username' => 'root', 'password' => 'root'
+	),		
+);
